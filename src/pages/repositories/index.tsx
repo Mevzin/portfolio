877bd9f9ea77fa1a5d9695a31b7b6a3c85cbc5c1
Thiago Torres de Souza <thiagomev@gmail.com>
2021-12-26 19:03:30 -0300

    Add spinner

--- src/pages/repositories/index.tsx
+++ src/pages/repositories/index.tsx
@@ -2,15 +2,18 @@ import { useEffect, useState } from "react";
 import { CardRepositories } from "../../components/CardRepositories";
 import { RepositoriesList } from "../../models/repositories.model";
 import { githubApi } from "../../services/githubApi";
+import {Spinner} from "react-bootstrap";
 
 export function Repositories(){
   const [repositories, setRepositories] = useState<RepositoriesList>((Object));
-
+  const [loading, setLoading] = useState(1);
+  
   useEffect(() => {
     async function loadRepositories() {
       await githubApi.get('/users/Mevzin/repos')
         .then((res: { data: RepositoriesList; }) => {
           setRepositories(res.data)
+          setLoading(0)
         })
     }
     loadRepositories();
@@ -20,7 +23,12 @@ export function Repositories(){
   return(
     
     <>
+    {
+      loading ? (
+        <Spinner animation="border" variant="danger"/>
+      ):(
       <CardRepositories repositories={repositories}/>
+      )}
     </>
   );
 }
