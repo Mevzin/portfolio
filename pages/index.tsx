93d3042e59cb6dad04e2e12c6558330ea114b8e6
Thiago Trres <thiagomev@gmail.com>
2022-04-10 15:52:27 -0300

    Banner finished

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,20 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable @next/next/no-img-element */
+import styles from "./home.module.scss";
 
-const Home: NextPage = () => {
-  return (<>
-    <h1>Home</h1>
-  </>)
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.bannerContainer}>
+        <section className={styles.text}>
+          <h1>
+            Hi my name is
+            <span> Thiago Torres </span>
+            and I'm a developer Front End
+          </h1>
+        </section>
+        <img src="/assets/profile.png" alt="Profile photo" />
+      </div>
+    </div>
+  );
 }
-
-export default Home
