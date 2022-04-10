53c87c9f2f06b1f52e0520fd8c381d6518ff29e4
Thiago Trres <thiagomev@gmail.com>
2022-04-10 16:33:56 -0300

    Add zomm efect in banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable @next/next/no-img-element */
+import { Zoom } from "react-awesome-reveal";
 import styles from "./home.module.scss";
+
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.bannerContainer}>
+      <Zoom 
+      cascade
+      duration={500}>
         <section className={styles.text}>
           <h1>
             Hi my name is
@@ -14,6 +19,7 @@ export default function Home() {
           </h1>
         </section>
         <img src="/assets/profile.png" alt="Profile photo" />
+      </Zoom>
       </div>
     </div>
   );
