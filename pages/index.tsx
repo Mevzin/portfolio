6f0e148d9b4a70e900d94f0fdb86b7734cf02d5f
Thiago Trres <thiagomev@gmail.com>
2022-04-10 17:28:29 -0300

    Add modifications on banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable @next/next/no-img-element */
-import { Zoom } from "react-awesome-reveal";
 import styles from "./home.module.scss";
 
 
@@ -8,10 +7,8 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.bannerContainer}>
-      <Zoom 
-      cascade
-      duration={500}>
         <section className={styles.text}>
+        
           <h1>
             Hi my name is
             <span> Thiago Torres </span>
@@ -19,7 +16,6 @@ export default function Home() {
           </h1>
         </section>
         <img src="/assets/profile.png" alt="Profile photo" />
-      </Zoom>
       </div>
     </div>
   );
