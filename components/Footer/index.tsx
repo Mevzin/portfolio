6c63cfe1b824aac8b63d654d78d92d7bb300cde4
Thiago Trres <thiagomev@gmail.com>
2022-04-10 21:28:47 -0300

    Add footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,9 +1,11 @@
 import styles from "./styles.module.scss";
 
-export default function Header() {
+export default function Footer() {
   return (
-    <div className={styles.navContainer}>
-      
+    <div className={styles.footerContainer}>
+      <p>
+        © 2022 <a href="https://github.com/mevzin" target="blank">Mevzin</a>.
+      </p>
     </div>
   );
 }
