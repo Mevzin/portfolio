24821a36274d31136920473fa4abe5ab86d50266
Thiago Trres <thiagomev@gmail.com>
2022-04-11 22:40:50 -0300

    Resolving error vercel

--- components/ContactMe/index.tsx
+++ components/ContactMe/index.tsx
@@ -8,8 +8,8 @@ export default function ContactMe() {
     e.preventDefault();
     emailjs
       .sendForm(
-        process.env.NEXT_PUBLIC_SERVICE_ID,
-        process.env.NEXT_PUBLIC_TEMPLATE_ID,
+        process.env.NEXT_PUBLIC_SERVICE_ID as string,
+        process.env.NEXT_PUBLIC_TEMPLATE_ID as string,
         e.target,
         process.env.NEXT_PUBLIC_USER_ID
       )
