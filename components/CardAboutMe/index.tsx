769f364ac84fb208d71e77172361e9794e853e24
Thiago Trres <thiagomev@gmail.com>
2022-04-13 12:09:02 -0300

    Modfy text about me

--- components/CardAboutMe/index.tsx
+++ components/CardAboutMe/index.tsx
@@ -8,10 +8,11 @@ export default function CardAboutMe() {
         <p>
           {" "}
           Ola meu nome é Thiago Torres, tenho 22 anos
-          moro em Iguatu CE, trabalho com front end há 3 anos
-          sou formado em Analise e Desenvolvimento de sistemas
-          pela Universidade Vale do Salgado e estou sempre em busca
-          de aprender mais e evoluir tanto como pessoa quanto como dev!
+          moro em Iguatu CE, sou formado em analise e desenvolvimento
+          de sistemas pela Universidade Vale do Salgado e trabalho com
+          front end há 3 anos e com back end a menos de 1 ano. Estou
+          sempre buscando aprender e melhorar minhas habilidades para
+          evoluir tanto como pessoa como também dev.
         </p>
         <a href="https://www.linkedin.com/in/thiagotorresmev/" target="blank">LinkedIn</a>
       </div>
